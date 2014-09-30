627ee24089aab4b05d1b99bf3ac654ca4c2910d2
Matthew <mvernhein@gmail.com>
2014-09-30 12:42:56 -0500

    Make a basic user model w/ secure passwords

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,12 +2,16 @@ require 'spec_helper'
 
 describe "user" do 
 
-	before { @user = User.new(name: "Example User", email: "user@example.com") }
+	before { @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar") }
 
 	subject { @user }
 
 	it { should respond_to(:name) }
 	it { should respond_to(:email) }
+	it { should respond_to(:password_digest) }
+	it { should respond_to(:password) }
+	it { should respond_to(:password_confirmation) }
+	it { should respond_to(:authenticate) }
 
 	it { should be_valid }
 
@@ -54,6 +58,37 @@ describe "user" do
       user_with_same_email.save
     end
 
-    it { should_not be_valid }
+    it { should be_invalid }
+  end
+
+  describe "when a password is not present" do 
+  	before { @user = User.new(name: "Example User", email: "user@example.com", password:" ", password_confirmation: " ") }
+  	it { should be_invalid }
+  end
+
+  describe "when a password does not match the password confirmation" do 
+  	before { @user.password_confirmation = "mismatch" }
+  	it { should be_invalid }
+  end
+
+  describe "with a password that's too short" do 
+  	before { @user.password = @user.password_confirmation = "a" * 5 }
+  	it { should be_invalid }
+  end
+
+  describe "return value of authenticate" do 
+  	before { @user.save }
+  	let(:found_user) { User.find_by(email: @user.email) }
+
+  	describe "with valid password" do 
+  		it { should eq found_user.authenticate(@user.password) }
+  	end
+
+  	describe "with invalid password" do 
+  		let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+  		it { should_not eq user_for_invalid_password }
+  		specify { expect(user_for_invalid_password).to be_false }
+  	end
   end
 end
