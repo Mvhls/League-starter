88b89f7bed445250c820e32a1aa50b17df39d247
Matthew <mvernhein@gmail.com>
2014-10-14 19:47:25 -0500

    created league index and show pages. WOP

--- spec/requests/league_pages_spec.rb
+++ spec/requests/league_pages_spec.rb
@@ -4,6 +4,30 @@ describe "league pages" do
 
 	subject { page }
 
-	describe "league page" do 
+	let(:user) { FactoryGirl.create(:user) }
+
+	describe "when visiting the index" do 
+		before do 
+			league = user.leagues.create(name: "example league")
+			visit leagues_path
+		end
+
+		it { should have_title("Leagues") }
+		it { should have_content("Leagues")}
+		it { should have_content("example league")}
 	end
+
+	
+	describe "when visiting a league page" do 
+
+
+		before do 
+			league = user.leagues.create(name: "example league")
+			visit league_path(league.id)
+		end
+
+		it { should have_title("example league") }
+		it { should have_content("example league") }
+	end
+
 end
