49c671171d0da2218e229f68498d6d056c9244eb
Matthew <mvernhein@gmail.com>
2014-11-26 19:19:57 -0600

    finished with team functions for now

--- spec/requests/team_pages_spec.rb
+++ spec/requests/team_pages_spec.rb
@@ -5,10 +5,11 @@ describe "team pages" do
 	subject { page }
 
 	create_user_league
+	let(:team) { league.teams.create(name: "exmaple league 2")}
 
 	describe "when visiting the index" do 
 		before do 
-			team = league.teams.create(name: "example team" )
+			league.teams.create(name: "example team")
 			visit league_teams_path(league)
 		end
 
@@ -16,6 +17,13 @@ describe "team pages" do
 		it { should have_content("example team") }
 		it { should have_content("example league") }
 
+	end
+
+	describe "when visiting the team page" do 
+		before do 
+			visit league_team_path(league, team.id)
+		end
+		it { should have_title("#{team.name}")}
 	end
 
 	describe "when on the create team page" do 
@@ -42,11 +50,9 @@ describe "team pages" do
 
 	describe "when looking at the teams in the league pages" do 
 		before do 
+			league.teams.create(name: "example team")
 			sign_in(user)
 			visit league_path(league)
-			click_link "Create a team"
-			fill_in('Name', :with => 'example team')
-			click_button('Create')
 		end
 
 		describe "as the commissioner" do 
