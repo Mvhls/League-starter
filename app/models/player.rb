d6fad582052ac1419f1b83aefc82a77c448e03a8
Matthew <mvernhein@gmail.com>
2015-01-10 02:29:34 -0600

    related stats to players and teams through polymorphic association

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,6 +2,7 @@ class Player < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :team
 	belongs_to :league
+	has_many :stats, as: :source_id
 
 	validates :team_id, presence: true
 	validates :league_id, presence: true
--- app/models/stat.rb
+++ app/models/stat.rb
@@ -1,2 +1,3 @@
 class Stat < ActiveRecord::Base
+	belongs_to :source_id, polymorphic: true
 end
